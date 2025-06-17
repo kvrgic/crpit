3f38a05f146299eb449abfc9534d74aef26c932a
Aida Kvrgić <kvrgicaida95@gmail.com>
2025-06-17 15:51:03 +0200

    fix: update frontend url

--- frontend/src/redux/features/comments/commentApi.js
+++ frontend/src/redux/features/comments/commentApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const commentApi = createApi({
     reducerPath: "commentApi", 
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000/api/comments',
+        baseUrl: 'https://crpit-backend.onrender.com/api/comments',
         credentials: "include"
     }),
     tagTypes: ['Comments'],
